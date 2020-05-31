15a589e4da6faf8b7ed8c27cb6abdfeee6b59ce9
Vitalii Lymar <vlm191997@gmail.com>
2020-05-31 21:55:49 +0300

    Set up cells in table

--- 100DaysOfSwiftProject1/TableViewController.swift
+++ 100DaysOfSwiftProject1/TableViewController.swift
@@ -10,5 +10,40 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-  
+  var picturesArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        for item in items {
+            if item.hasPrefix("nssl") {
+                picturesArray.append(item)
+                print(item)
+            }
+        }
+
+    }
+    
+    // MARK: - Table view data source
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return picturesArray.count
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.textLabel?.text = picturesArray[indexPath.row]
+        
+        return cell
+    }
+    
+
 }
