b01309bfb98c8fafdc0a10a3d4b2bcbed8e56791
Vitalii Lymar <vlm191997@gmail.com>
2020-05-31 21:48:57 +0300

    Create Detail VC

---
+++ 100DaysOfSwiftProject1/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  100DaysOfSwiftProject1
+//
+//  Created by HexaHack on 31.05.2020.
+//  Copyright © 2020 HexaHack. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+
+}
